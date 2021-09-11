63666819f4eb965f3f9bb6432098e60a91f45fa8
kishoregekkula <k.gekkula@gmail.com>
2021-09-11 19:53:54 +0530

    PWM test 1

---
+++ pwm_test.js
@@ -0,0 +1,17 @@
+/**
+ * Created by kishoregekkula on 11/09/21.
+ */
+const Gpio = require('pigpio').Gpio;
+
+const led = new Gpio(17, {mode: Gpio.OUTPUT});
+
+let dutyCycle = 0;
+
+setInterval(() => {
+    led.pwmWrite(dutyCycle);
+
+    dutyCycle += 5;
+    if (dutyCycle > 255) {
+        dutyCycle = 0;
+    }
+}, 20);
